0b0d82a746e22d4a95536211e8f3b89b3c4c2cdf
Ovyerus <Ovyerus@users.noreply.github.com>
2017-09-27 11:54:55 +1000

    Change form-data lib, and actually get results now

--- tests/url.js
+++ tests/url.js
@@ -10,7 +10,7 @@ const sourcer = new Sagiri(token);
 
 console.log('Starting url test...');
 
-sourcer.getSource('http://i.imgur.com/5yFTeRV.png').then(console.log).catch(err => {
+sourcer.getSource('http://cfile29.uf.tistory.com/image/277D9B3453F9D9283659F4').then(console.log).catch(err => {
     console.error('File test failed!');
     console.error(err);
     process.exit(1);
