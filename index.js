b21115c9edada5b67c618d1e5a7d5b2577da3225
Ovyerus <Ovyerus@users.noreply.github.com>
2017-10-17 18:00:33 +1100

    Add proper ratelimiting, and reorganise some things

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-module.exports = require('./lib/Sagiri');
+const Handler = require('./lib/Handler');
+const Constants = require('./lib/Constants');
+const Ratelimiter = require('./lib/Ratelimiter');
+
+function Sagiri(key, options) {
+    return new Handler(key, options);
+}
+
+Sagiri.Handler = Handler;
+Sagiri.Constants = Constants;
+Sagiri.Ratelimiter = Ratelimiter;
+
+module.exports = Sagiri;
