aa3695581e0543c71f795fca2304c55f483588fb
Capuccino <chinodesuuu@gmail.com>
2017-10-23 03:44:40 +0000

    [wub]

---
+++ tests/mask.js
@@ -0,0 +1,18 @@
+/**
+ * @file testing the Masking parameters in SauceNao
+ * @author Capuccino
+ * @license MIT
+ **/
+ 
+ const Handler = require('../');
+ const token = require('./token.json').token;
+ const s = new Handler(token, {
+     // disable site index for this test
+     dbMaskI: 0,
+     dbMask: 10,
+     testMode: 1,
+     outputType: 2
+ });
+ 
+ //one-liners ecksde
+ s.getSauce('https://cdn.awwni.me/zqqd.jpg').then(res => console.log(res)).catch(err => console.error(err));
