ab03ec3010b527f87d62248781f47044ee40b5ed
Ovyerus <iamovyerus@gmail.com>
2019-10-24 19:27:56 +1100

    [jest.config] Remove weird options

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,4 @@
 module.exports = {
-  displayName: 'ts-jest',
   preset: 'ts-jest',
-  testEnvironment: 'node',
-  testMatch: ['<rootDir>/tests/*.test.ts'],
-  setupFiles: ['<rootDir>/tests/jest.setup.ts']
+  testEnvironment: 'node'
 };
