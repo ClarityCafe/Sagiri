07b46ba0e5fc7ccbc7f37eb5709035f52cf1639f
Ayane Satomi <chinodesuuu@gmail.com>
2020-06-09 10:09:27 +0800

    sites: change to @typescript-eslint/naming-convention

--- lib/sites.ts
+++ lib/sites.ts
@@ -1,4 +1,4 @@
-/* eslint-disable @typescript-eslint/camelcase */
+/* eslint-disable @typescript-eslint/naming-convention */
 import { Result, ResultData } from "./response";
 
 interface AuthorData {
