5c74af603208384af3a5c3c9a513ae2496fd11c0
Ayane Satomi <chinodesuuu@gmail.com>
2020-06-09 10:08:39 +0800

    index: Change to @typescript-eslint/naming-convention

--- lib/index.ts
+++ lib/index.ts
@@ -1,4 +1,4 @@
-/* eslint-disable @typescript-eslint/camelcase */
+/* eslint-disable @typescript-eslint/naming-convention */
 import bent from "bent";
 import debug from "debug";
 import FormData from "form-data";
