d16044ea9c84bdaaff5a7248c5df4bbebc83bb69
chinodesuuu <chinodesuuu@gmail.com>
2024-08-27 14:41:19 +0000

    somehow I'm confused assertIsError is different from assertThrows wtf
    Signed-off-by: chinodesuuu <chinodesuuu@gmail.com>

--- test/deno/sagiri.test.ts
+++ test/deno/sagiri.test.ts
@@ -1,14 +1,14 @@
-import { assertIsError, assertExists } from "@std/assert";
+import { assertThrows, assertExists } from "@std/assert";
 import sagiri from "../../lib/sagiri.ts";
 
 Deno.test("should fail on invalid characters", () => {
-  assertIsError(sagiri("!!!!!*&#@(!)"));
+  assertThrows(() => { sagiri("!!!!!*&#@(!)") });
 });
 
 Deno.test("should fail on invalid length", () => {
-  assertIsError(sagiri("7".repeat(27)));
+  assertThrows(() => { sagiri("7".repeat(27)) });
 });
 
 Deno.test("Resolve with results", () => {
-  assertExists(()=> {sagiri(Deno.env.get("SAUCENAO_TOKEN")!)("https://i.imgur.com/F9QSgPx.jpeg")}, "pixiv");
+  assertExists(() => { sagiri(Deno.env.get("SAUCENAO_TOKEN")!)("https://i.imgur.com/F9QSgPx.jpeg") }, "pixiv");
 })
